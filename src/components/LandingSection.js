342c44886afcccb56cdbc8693f996bc5c4e7f283
lucacasty <luca.castelli02@gmail.com>
2025-04-01 11:13:48 +0200

    Adding avatar and heading on landing page

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Luca!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,6 +15,22 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+
+  <Avatar name="Castelli Luca" src="https://i.postimg.cc/8cD3MjFv/Webp-net-resizeimage-3.jpg" size="4"  color="cyan" objectFit="cover" />
+  <VStack spacing={8} mt={8}>
+    <Heading as="h1" size="md" color="white">
+      {greeting}
+    </Heading>
+    <Heading as="h2" size="2xl" color="white">
+      {bio1}  
+      <VStack spacing={0} mt={2}> 
+        <Heading as="h2" size="2xl" color="white">
+          {bio2}  
+        </Heading>  
+      </VStack>
+    </Heading>
+  </VStack>
+
 
   </FullScreenSection>
 );
